c8620c112d37e5eb769f2053dcc8ae5711442219
EsraDNDR <esradndr34@gmail.com>
2013-01-18 12:42:58 +0200

    remain21

--- app/models/loan.rb
+++ app/models/loan.rb
@@ -3,3 +3,10 @@ class Loan < ActiveRecord::Base
 	
   attr_accessible :credit, :debt, :due_time, :friend_id, :from_time
 end
+#<p>
+ # <b>Remaining Loan:</b>
+  #<% if @remainLoan <= 0 or @remainLoan == nil %>
+#  <% else %>
+   # <%= @remainLoan %>
+ # <% end %>
+#</p>
