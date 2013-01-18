a0125fcd93e567ee41cb35c479b9048aa3ea05ed
EsraDNDR <esradndr34@gmail.com>
2013-01-18 11:23:35 +0200

    deneme4

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 OmniAuth.config.logger = Rails.logger
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :twitter, 'TAZWh3T8tx2yhZqEzrEPw', '3lbLfUyzxL20py8NJNpuQEcWk6Gtp95PBdodR8uQs', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } } 
+  #provider :twitter, 'TAZWh3T8tx2yhZqEzrEPw', '3lbLfUyzxL20py8NJNpuQEcWk6Gtp95PBdodR8uQs', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } } 
   provider :facebook, '139301336228321', '7a26f49b382fe5b3a9cc694dc314d8be', :client_options => { :ssl => { :ca_file => "#{Rails.root}/config/ca-bundle.crt" } } 
 end
